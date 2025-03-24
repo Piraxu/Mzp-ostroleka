75315a87a4f663e31ee13d9dae6d1220d44c8de5
Piraxu <125399409+Piraxu@users.noreply.github.com>
2025-03-24 17:02:01 +0100

    Update links.js

--- timetables/9/links.js
+++ timetables/9/links.js
@@ -29,17 +29,3 @@ function mlinks() {
         sinfo.setAttribute("href", actuallink+"info.html")
     }
 
-function timestamp() {
- document.addEventListener("DOMContentLoaded", function() {
-            const stopLinks = document.querySelectorAll(".stop-element, .stop-element-side");
-            stopLinks.forEach(link => {
-                link.addEventListener("click", function(event) {
-                    event.preventDefault();
-                    const originalHref = link.getAttribute("href");
-                    link.setAttribute("href", `${originalHref}?t=${Date.now()}`);
-                    window.location.href = link.getAttribute("href");
-                });
-            });
-        });
-
-}
